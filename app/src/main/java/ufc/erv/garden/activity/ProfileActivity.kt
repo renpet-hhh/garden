8066dad000841d3e257e22f07156631ba320ea49
Elvin <elvinmatheussousa@gmail.com>
2022-06-26 11:15:59 -0300

    tela perfil criada

---
+++ app/src/main/java/ufc/erv/garden/activity/ProfileActivity.kt
@@ -0,0 +1,12 @@
+package ufc.erv.garden.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import ufc.erv.garden.R
+
+class ProfileActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.profile)
+    }
+}
